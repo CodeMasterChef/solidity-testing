8743c3d8bf744272684ecc069134fd4cdf64e6b9
Ninh Le <levanninh0101@gmail.com>
2021-04-16 08:08:02 +0700

    test case

--- test/AdvanceStorage.test.js
+++ test/AdvanceStorage.test.js
@@ -1,13 +1,30 @@
 const AdvancedStorage = artifacts.require('AdvancedStorage');
 
 contract('AdvancedStorage', () => {
-    it('Should add element to ids array', async () => {
-        const advancedStorage = await AdvancedStorage.deployed();
-        const value = 12;
-        const element = await advancedStorage.ids(0);
-        assert.equal(element, value);
-        // OR using toNumber() for big number
-        //assert(element.toNumber() === value);
-    })
+  it('Should add element to ids array', async () => {
+    const advancedStorage = await AdvancedStorage.deployed();
+    const value = 10;
+    await advancedStorage.add(value);
+    const element = await advancedStorage.ids(0);
+    assert.equal(element, value);
+    // OR using toNumber() for big number
+    //assert(element.toNumber() === value);
+  })
+
+  it('Should get element from ids array', async () => {
+    const advancedStorage = await AdvancedStorage.deployed();
+    const value = 20;
+    await advancedStorage.add(value);
+    const element = await advancedStorage.get(1);
+    assert.equal(element.toNumber(), value);
+  })
+
+  it('Should return length of ids array', async () => {
+    const advancedStorage = await AdvancedStorage.deployed();
+    const length = await advancedStorage.length();
+    assert.equal(length, 2);
+  })
+
+
 
 })
