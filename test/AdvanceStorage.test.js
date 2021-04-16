d38298760ee3fab013a916f55648d04cb7d75749
Ninh Le <ninh.le@loyaworld.com>
2021-04-16 16:20:12 +0700

    update test case

--- test/AdvanceStorage.test.js
+++ test/AdvanceStorage.test.js
@@ -1,10 +1,14 @@
 const AdvancedStorage = artifacts.require('AdvancedStorage');
 
 contract('AdvancedStorage', () => {
+    let advancedStorage = null;
+    let id1 = 10;
+    let id2 = 20;
+    before(async () => {
+        advancedStorage = await AdvancedStorage.deployed();
+    })
     it('Should add element to ids array', async () => {
-        const advancedStorage = await AdvancedStorage.deployed();
-        const id = 10;
-        await advancedStorage.add(id);
+        await advancedStorage.add(id1);
         const element = await advancedStorage.ids(0);
         // Use toNumber() for BigNumber (uint from Solidity will return the BigNumber)
         /* BN {
@@ -13,30 +17,26 @@ contract('AdvancedStorage', () => {
             length: 1,
             red: null
           } */
-        assert.equal(element.toNumber(), id);
-        // OR use like below because .equal() supports BN comparation.
+        assert.equal(element.toNumber(), id1);
+        // OR use like below because the .equal() supports BN comparation.
         // assert.equal(element, id);
     })
 
     it('Should return an element from ids array', async () => {
-        const advancedStorage = await AdvancedStorage.deployed();
-        const id = 20;
-        await advancedStorage.add(id);
+        await advancedStorage.add(id2);
         const element = await advancedStorage.get(1);
-        assert.equal(element.toNumber(), id);
+        assert.equal(element.toNumber(), id2);
     })
 
 
     it('Should return all elements of ids array', async () => {
-        const advancedStorage = await AdvancedStorage.deployed();
         const rawIds = await advancedStorage.getAll();
         // convert BigNumber[] to Javacript array (number[])
         const ids = rawIds.map(id => id.toNumber());
-        assert.deepEqual(ids, [10, 20]);
+        assert.deepEqual(ids, [id1, id2]);
     })
 
     it('Should return the length of ids array', async () => {
-        const advancedStorage = await AdvancedStorage.deployed();
         const length = await advancedStorage.length();
         assert.equal(length, 2);
     })
